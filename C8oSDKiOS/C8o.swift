0b2efea11a4bf1282e6de95057e4568c8abe718f
Charles Grimont <charlesg@convertigo.com>
2016-02-04 14:35:14 +0000

    InProgress #4932 - Create the Swift project for the Client SDK 2.0 iOS
    Share project files

---
+++ C8oSDKiOS/C8o.swift
@@ -0,0 +1,44 @@
+//
+//  C8o.swift
+//  C8oSDKiOS
+//
+//  Created by Charles Grimont on 03/02/2016.
+//  Copyright © 2016 Convertigo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+import CouchbaseLite
+
+public class C8o {
+    
+    public init() {
+        
+        
+        
+        print("Hello C8o SDK!");
+        
+        let manager = CBLManager();
+        
+        print("Manager initialized");
+
+       
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
+        
+    }
+    
+    
+    
+}
---
+++ C8oSDKiOSTests/C8oSDKiOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  C8oSDKiOSTests.swift
+//  C8oSDKiOSTests
+//
+//  Created by Charles Grimont on 03/02/2016.
+//  Copyright © 2016 Convertigo. All rights reserved.
+//
+
+import XCTest
+@testable import C8oSDKiOS
+
+class C8oSDKiOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
