3280c970cddadd2e0beb2798b16c57b05aabbd64
CharlesGrimont <grimont.c@gmail.com>
2017-06-30 14:09:19 +0200

    modifying briding header

--- C8oSDKiOS/Couchbase-Lite-Bridging-Header.h
+++ C8oSDKiOS/Couchbase-Lite-Bridging-Header.h
@@ -1,12 +1,16 @@
 #ifndef Couchbase_Lite_Bridging_Header_h
 #define Couchbase_Lite_Bridging_Header_h
 
+#ifndef CBLRegisterJSViewCompiler_h
+#define CBLRegisterJSViewCompiler_h
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnullability-completeness"
 
 #import "CBLRegisterJSViewCompiler.h"
 
 #pragma clang diagnostic pop
+#endif
 #endif
 
 
