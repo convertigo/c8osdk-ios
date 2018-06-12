93d69967aa99fe13b9ccdaeed4036a7c650ed3e4
Charles Grimont <grimont.c@gmail.com>
2018-06-12 14:22:03 +0200

    add sources

---
+++ Pods/Target Support Files/Pods-C8oSDKiOS/Pods-C8oSDKiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_C8oSDKiOS : NSObject
+@end
+@implementation PodsDummy_Pods_C8oSDKiOS
+@end
---
+++ Pods/Target Support Files/Pods-C8oSDKiOSTests/Pods-C8oSDKiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_C8oSDKiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_C8oSDKiOSTests
+@end
